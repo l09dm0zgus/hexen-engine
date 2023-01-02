cea0a6b6b8dbcc57faec2a4f97730a39444e2802
cx9ps3 <cx9ps3@gmail.com>
2023-01-02 23:01:17 +0200

    added RectangleVertices for render rectangle .

---
+++ app/jni/src/core/graphics/RectangleVertices.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cx9ps3 on 02.01.23.
+//
+
+#include "RectangleVertices.h"
+
+float *RectangleVertices::data()
+{
+    return vertices;
+}
---
+++ app/jni/src/core/graphics/RectangleVertices.h
@@ -0,0 +1,26 @@
+//
+// Created by cx9ps3 on 02.01.23.
+//
+
+#ifndef BATTLEBLAZE_RECTANGLEVERTICES_H
+#define BATTLEBLAZE_RECTANGLEVERTICES_H
+
+
+class RectangleVertices
+{
+    float *data();
+private:
+    float vertices[18]
+    {
+        -0.5f,-5.0f,0.0f,
+        -0.5f,0.5f,0.0f,
+        0.5f,0.5f,0.5f,
+        0.5f,0.5f,0.5f,
+        0.5f,-0.5f,0.5f,
+        -0.5f,-5.0f,0.0f,
+
+    };
+};
+
+
+#endif //BATTLEBLAZE_RECTANGLEVERTICES_H
