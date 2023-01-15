e2beb02d9fe6e0af184ef992696cc9812b0f3a96
cx9ps3 <cx9ps3@gmail.com>
2023-01-15 02:41:36 +0200

    moved to sdl3

--- app/jni/src/main.cpp
+++ app/jni/src/main.cpp
@@ -1,8 +1,9 @@
 //
 // Created by cx9ps3 on 22.12.22.
 //
+#define main SDL_main
 #include "core/application/Application.h"
-int main(int argc, char *argv[])
+extern "C" __attribute__((visibility("default"))) int SDL_main(int argc, char** argv)
 {
     Application application;
     application.run();
