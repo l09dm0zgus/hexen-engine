fd03c69ee6c11954786ef529d719c0a702f37358
cx9ps3 <cx9ps3@gmail.com>
2023-01-28 01:03:31 +0200

    added uuid

---
+++ app/jni/src/core/uuid/uuid.h
@@ -0,0 +1,45 @@
+//
+// Created by cx9ps3 on 28.01.23.
+//
+
+#ifndef BATTLEBLAZE_UUID_H
+#define BATTLEBLAZE_UUID_H
+#include <random>
+#include <sstream>
+
+
+
+std::string generateUUIDV4()
+{
+    std::stringstream ss;
+    int i;
+    ss << std::hex;
+    for (i = 0; i < 8; i++)
+    {
+        ss << dis(gen);
+    }
+    ss << "-";
+    for (i = 0; i < 4; i++)
+    {
+        ss << dis(gen);
+    }
+    ss << "-4";
+    for (i = 0; i < 3; i++)
+    {
+        ss << dis(gen);
+    }
+    ss << "-";
+    ss << dis2(gen);
+    for (i = 0; i < 3; i++)
+    {
+        ss << dis(gen);
+    }
+    ss << "-";
+    for (i = 0; i < 12; i++)
+    {
+        ss << dis(gen);
+    };
+    return ss.str();
+}
+
+#endif //BATTLEBLAZE_UUID_H
