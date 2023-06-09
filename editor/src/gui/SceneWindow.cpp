a7860577ce93e214f4b78f8d76e3c659a0b1b452
l09dm0zgus <cx9ps3@gmail.com>
2023-06-09 22:33:13 +0300

    added window for drawing SceneWindow

---
+++ editor/src/gui/SceneWindow.cpp
@@ -0,0 +1,65 @@
+//
+// Created by cx9ps3 on 09.06.2023.
+//
+
+#include "SceneWindow.h"
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <iostream>
+#include <GL/glew.h>
+
+edit::gui::SceneWindow::SceneWindow(const std::string &name) : GUIWindow(name)
+{
+    frameBufferTexture = core::mem::make_unique<core::rend::FrameBufferTexture>(glm::vec2(1280,720));
+    frameBufferObject.bindRenderBuffer();
+    frameBufferObject.setSize(glm::vec2(1280,720));
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n";
+    }
+    frameBufferObject.unbind();
+    frameBufferTexture->unbind();
+    frameBufferObject.unbindRenderBuffer();
+
+}
+
+void edit::gui::SceneWindow::draw()
+{
+    ImGui::Begin(getName().c_str());
+    {
+        // Using a Child allow to fill all the space of the window.
+        // It also alows customization
+        ImGui::BeginChild("GameRender");
+        // Get the size of the child (i.e. the whole draw size of the windows).
+        ImVec2 windowSize = ImGui::GetWindowSize();
+        // Because I use the texture from OpenGL, I need to invert the V from the UV.
+        ImGui::Image((ImTextureID)frameBufferTexture->getID(), windowSize, ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::EndChild();
+    }
+    ImGui::End();
+}
+
+void edit::gui::SceneWindow::end()
+{
+    frameBufferObject.bind();
+    draw();
+    frameBufferObject.unbind();
+}
+
+void edit::gui::SceneWindow::begin()
+{
+
+}
+
+void edit::gui::SceneWindow::clear()
+{
+    // Set background color as cornflower blue
+    glClearColor(0.39f, 0.58f, 0.93f, 1.f);
+    // Clear color buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void edit::gui::SceneWindow::drawScene()
+{
+    clear();
+}
---
+++ editor/src/gui/SceneWindow.h
@@ -0,0 +1,34 @@
+//
+// Created by cx9ps3 on 09.06.2023.
+//
+
+#ifndef HEXENEDITOR_SCENEWINDOW_H
+#define HEXENEDITOR_SCENEWINDOW_H
+
+#include <core/Types.h>
+#include <core/graphics/FrameBufferObject.h>
+#include <core/graphics/texture/FrameBufferTexture.h>
+#include "GUIWindow.h"
+
+namespace edit::gui
+{
+    class SceneWindow : public GUIWindow
+    {
+    public:
+        SceneWindow(const std::string &name);
+        void begin() override;
+        void draw() override;
+        void end() override;
+    private:
+        void clear();
+        void drawScene();
+        core::rend::FrameBufferObject frameBufferObject;
+        std::unique_ptr<core::rend::FrameBufferTexture> frameBufferTexture;
+    };
+
+}
+
+
+
+
+#endif //HEXENEDITOR_SCENEWINDOW_H
