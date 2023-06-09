330d170893f287106da96c960dd647e7b47f5e44
l09dm0zgus <cx9ps3@gmail.com>
2023-06-09 19:52:21 +0300

    added frame buffer texture

---
+++ engine/src/core/graphics/texture/FrameBufferTexture.h
@@ -0,0 +1,25 @@
+//
+// Created by cx9ps3 on 09.06.2023.
+//
+
+#ifndef HEXENEDITOR_FRAMEBUFFERTEXTURE_HPP
+#define HEXENEDITOR_FRAMEBUFFERTEXTURE_HPP
+
+#include <glm/vec2.hpp>
+#include "../../memory_pool/AllocatedObject.h"
+
+namespace core::rend
+{
+    class FrameBufferTexture : mem::AllocatedObject
+    {
+    private:
+        u32 object{0};
+    public:
+        explicit FrameBufferTexture(const glm::vec2 &size);
+        ~FrameBufferTexture();
+        u32 getID() const noexcept;
+        void resize(const glm::vec2 &size) const;
+    };
+}
+
+#endif //HEXENEDITOR_FRAMEBUFFERTEXTURE_HPP
