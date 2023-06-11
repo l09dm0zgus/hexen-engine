b81053093505061ff5ba41135fa47e8854c491b8
l09dm0zgus <cx9ps3@gmail.com>
2023-06-11 17:33:44 +0300

    created interface for File dialogs

---
+++ editor/src/gui/editors_windows/native_file_dialog/INativeFileDialog.h
@@ -0,0 +1,36 @@
+//
+// Created by cx9ps3 on 11.06.2023.
+//
+
+#ifndef HEXENEDITOR_INATIVEFILEDIALOG_H
+#define HEXENEDITOR_INATIVEFILEDIALOG_H
+
+#include <core/Types.h>
+#include <string>
+
+namespace edit::gui
+{
+    class INativeFileDialog
+    {
+    public:
+        virtual ~INativeFileDialog() = default;
+        INativeFileDialog(INativeFileDialog &&iNativeFileDialog) = delete;
+        INativeFileDialog(const INativeFileDialog &iNativeFileDialog) = delete;
+        INativeFileDialog& operator=(INativeFileDialog &&iNativeFileDialog) = delete;
+        INativeFileDialog& operator=(const INativeFileDialog &iNativeFileDialog) = delete;
+
+        enum class Status : core::i32
+        {
+            OK,
+            ERROR,
+            CANCEL
+        };
+
+        virtual Status openDialog(const std::string &filterList,const std::string &defaultPath,std::string *pathToFile) = 0;
+        virtual Status openDialog(const std::string &filterList,const std::string &defaultPath,std::vector<std::string> &pathToFiles) = 0;
+        virtual Status saveDialog(const std::string &filterList,const std::string &defaultPath,std::string *pathToFile) = 0;
+        virtual Status pickDialog(const std::string &defaultPath,std::string *pathToFile) = 0;
+    };
+}
+
+#endif //HEXENEDITOR_INATIVEFILEDIALOG_H
