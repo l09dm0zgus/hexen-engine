bd9e2e3f643395b1f44c837c71bf6af8771337a1
l09dm0zgus <cx9ps3@gmail.com>
2023-06-15 20:43:31 +0300

    fixed compile errors

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,22 +1,10 @@
 //
 // Created by cx9ps3 on 24.05.2023.
 //
-#include <iostream>
 #include "application/Application.h"
-#include <filesystem>
-#include "../../../RealOne-Engine/editor/src/gui/file_dialog/FileDialog.h"
 
 int main()
 {
-    //edit::Application app;
-  //  app.run();
-  edit::gui::FileDialog fileDialog;
-  edit::gui::INativeFileDialog::PathSet pathSet;
-  edit::gui::INativeFileDialog::FileFilter fileFilter;
-  fileFilter.push_back({{"Executable"},{"dll;exe;"}});
-  fileDialog.openDialog(fileFilter,"",&pathSet);
-  for(const auto & path : pathSet.path)
-  {
-      std::cout << path << std::endl;
-  }
+    edit::Application app;
+    app.run();
 }
