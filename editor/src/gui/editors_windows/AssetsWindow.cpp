aea8bce7f321dae8452a34cbda0498bf6dc6e408
l09dm0zgus <cx9ps3@gmail.com>
2023-06-22 16:18:30 +0300

    moved loading fonts

--- editor/src/gui/editors_windows/AssetsWindow.cpp
+++ editor/src/gui/editors_windows/AssetsWindow.cpp
@@ -12,20 +12,6 @@ edit::gui::AssetsWindow::AssetsWindow(std::string name) : GUIWindow(std::move(na
 {
     setSize(glm::vec2(1280,400));
 
-    ImGuiIO& io = ImGui::GetIO();
-    io.Fonts->AddFontDefault();
-    float baseFontSize = 16.0f; // 13.0f is the size of the default font. Change to the font size you use.
-    float iconFontSize = baseFontSize * 2.0f / 3.0f; // FontAwesome fonts need to have their sizes reduced by 2.0f/3.0f in order to align correctly
-
-
-    static const ImWchar iconsRanges[] = { ICON_MIN_FA, ICON_MAX_16_FA, 0};
-
-    ImFontConfig iconsConfig;
-    iconsConfig.MergeMode = true;
-    iconsConfig.GlyphMinAdvanceX = iconFontSize;
-    iconsConfig.PixelSnapH = true;
-
-    io.Fonts->AddFontFromFileTTF( "fonts/fa-solid-900.ttf", iconFontSize,&iconsConfig,iconsRanges);
 
     currenPath.emplace_back("Folder1");
     currenPath.emplace_back("Folder2");
@@ -65,6 +51,14 @@ void edit::gui::AssetsWindow::draw()
     drawSoundButton("music.mp3");
     drawImageButtons();
     popButtonStyle();
+    if(ImGui::BeginPopupContextWindow())
+    {
+        if(ImGui::MenuItem("Lol"))
+        {
+
+        }
+        ImGui::EndPopup();
+    }
     ImGui::End();
 
 
