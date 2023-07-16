3a8995620cb7776a2b4135dda43365e088c27de9
l09dm0zgus <cx9ps3@gmail.com>
2023-07-16 16:58:07 +0300

    removed methods, fixed typos

--- engine/src/core/threading/TaskCounter.cpp
+++ engine/src/core/threading/TaskCounter.cpp
@@ -7,114 +7,24 @@
 #include "ThreadLocalStorage.h"
 
 
-core::threading::detail::BaseCounter::BaseCounter(core::threading::TaskManager *manager, core::u8 numberOfWaitingFibers,core::threading::detail::BaseCounter::WaitingFibers *waitingFibers,std::atomic_bool *freeWaitingSlots) : manager(manager) , nubmberOfWaitingFibers(numberOfWaitingFibers),waitingFibers(waitingFibers),freeWaitingSlots(freeWaitingSlots)
+core::threading::TaskCounter::TaskCounter(core::threading::TaskManager *manager) : manager(manager)
 {
 
 }
 
-void core::threading::detail::BaseCounter::initializeWaitingFibers()
-{
-    for(u8 i = 0;i < nubmberOfWaitingFibers;i++)
-    {
-        freeWaitingSlots[i].store(true);
-    }
-}
-
-void core::threading::detail::BaseCounter::checkWaitingFibers(core::u32 value)
-{
-    for(u64 i = 0;i < nubmberOfWaitingFibers;i++)
-    {
-        if(freeWaitingSlots[i].load(std::memory_order_acquire))
-        {
-            continue;
-        }
-
-        auto waitingSlot = &waitingFibers[i];
-        if(waitingSlot->inUse.load(std::memory_order_acquire))
-        {
-            continue;
-        }
-
-        if(waitingSlot->targetValue == value)
-        {
-            bool expected{false};
-
-            if(!std::atomic_compare_exchange_strong_explicit(&waitingSlot->inUse,&expected, true,std::memory_order_seq_cst,std::memory_order_relaxed))
-            {
-                continue;
-            }
-
-            manager->getCurrentTLS()->readyFibers.emplace_back(waitingSlot->fiberIndex, waitingSlot->fiberStored);
-            freeWaitingSlots[i].store(true, std::memory_order_release);
-        }
-    }
-}
-
-core::u32 core::threading::detail::BaseCounter::increment(core::u32 by)
+core::u32 core::threading::TaskCounter::increment(core::u32 by)
 {
     auto previous = counter.fetch_add(by);
-    checkWaitingFibers(previous + by);
     return previous;
 }
 
-core::u32 core::threading::detail::BaseCounter::decrement(core::u32 by)
+core::u32 core::threading::TaskCounter::decrement(core::u32 by)
 {
-    auto previous = counter.fetch_add(by);
-    checkWaitingFibers(previous - by);
+    auto previous = counter.fetch_sub(by);
     return previous;
 }
 
-core::u32 core::threading::detail::BaseCounter::getValue() const
+core::u32 core::threading::TaskCounter::getValue() const
 {
     return counter.load(std::memory_order_seq_cst);
-}
-
-bool core::threading::detail::BaseCounter::addWaitingFiber(core::u16 fiberIndex, core::u32 targetValue,std::atomic_bool *fiberStored) {
-    for (u8 i = 0; i < nubmberOfWaitingFibers; i++) {
-        // Acquire Free Waiting Slot
-        bool expected = true;
-        if (!std::atomic_compare_exchange_strong_explicit(&freeWaitingSlots[i], &expected, false,
-                                                          std::memory_order_seq_cst, std::memory_order_relaxed)) {
-            continue;
-        }
-
-        // Setup Slot
-        auto slot = &waitingFibers[i];
-        slot->fiberIndex = fiberIndex;
-        slot->fiberStored = fiberStored;
-        slot->targetValue = targetValue;
-
-        slot->inUse.store(false);
-
-        // Check if we are done already
-        auto c = counter.load(std::memory_order_relaxed);
-
-        if (slot->inUse.load(std::memory_order_acquire)) {
-            return false;
-        }
-
-        if (slot->targetValue == counter) {
-            expected = false;
-            if (!std::atomic_compare_exchange_strong_explicit(&slot->inUse, &expected, true, std::memory_order_seq_cst,
-                                                              std::memory_order_relaxed)) {
-                return false;
-            }
-
-            freeWaitingSlots[i].store(true, std::memory_order_release);
-            return true;
-        }
-
-        return false;
-    }
-    return false;
-}
-
-core::threading::TaskCounter::TaskCounter(core::threading::TaskManager *manager) : detail::BaseCounter(manager, MAX_WAITING, waitingFibers, freeWaitingSlots)
-{
-    initializeWaitingFibers();
-}
-
-core::threading::detail::TinyCounter::TinyCounter(core::threading::TaskManager *manager) : detail::BaseCounter(manager,1,&waitingFiber,&freeWaitingSlot)
-{
-    initializeWaitingFibers();
 }
--- engine/src/core/threading/TaskCounter.h
+++ engine/src/core/threading/TaskCounter.h
@@ -10,64 +10,22 @@
 namespace core::threading
 {
     class TaskManager;
-    namespace detail
+    class TaskCounter
     {
-        class BaseCounter
-        {
-        private:
-        protected:
-            friend class Manager;
-
-            std::atomic<u32> counter{0};
-
-            struct WaitingFibers
-            {
-                u16 fiberIndex = UINT16_MAX;
-                std::atomic_bool* fiberStored{nullptr};
-                u32 targetValue{0};
-                std::atomic_bool  inUse{true};
-            };
-
-            const u8 nubmberOfWaitingFibers;
-
-            WaitingFibers* waitingFibers{nullptr};
-            std::atomic_bool* freeWaitingSlots;
-
-            void initializeWaitingFibers();
-
-            TaskManager *manager;
-
-            void checkWaitingFibers(u32 value);
-
-        public:
-            BaseCounter(TaskManager* manager , u8 numberOfWaitingFibers,WaitingFibers* waitingFibers , std::atomic_bool* freeWaitingSlots);
-            virtual ~BaseCounter() = default;
-
-            u32 increment(u32 by = 1);
-            u32 decrement(u32 by = 1);
-
-            u32 getValue() const;
-
-            bool addWaitingFiber(u16 fiberIndex ,u32 targetValue,std::atomic_bool* fiberStored);
-        };
-        struct TinyCounter : public  BaseCounter
-        {
-            explicit TinyCounter(TaskManager *manager);
-            ~TinyCounter() override = default;
-
-            std::atomic_bool freeWaitingSlot{};
-            WaitingFibers waitingFiber;
-        };
-    }
-    class TaskCounter : public detail::BaseCounter
-    {
-    public:
-        static const constexpr  u8 MAX_WAITING = 4;
-        explicit TaskCounter(TaskManager *manager);
-        ~TaskCounter() override = default;
     private:
-        std::atomic_bool freeWaitingSlots[MAX_WAITING]{};
-        WaitingFibers waitingFibers[MAX_WAITING];
+        friend class Manager;
+
+        std::atomic<u32> counter{0};
+        TaskManager *manager;
+
+
+    public:
+        TaskCounter(TaskManager* manager);
+
+        u32 increment(u32 by = 1);
+        u32 decrement(u32 by = 1);
+
+        u32 getValue() const;
     };
 }
 
