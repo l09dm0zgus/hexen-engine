b28efaa720808da380b05de09249547c353a6199
l09dm0zgus <cx9ps3@gmail.com>
2023-07-18 23:11:09 +0300

    renamed class and added checking free fibers

---
+++ engine/src/core/threading/BaseCounter.cpp
@@ -0,0 +1,120 @@
+//
+// Created by cx9ps3 on 14.07.2023.
+//
+
+#include "BaseCounter.h"
+
+#include "TaskManager.h"
+
+
+core::threading::BaseCounter::BaseCounter(core::threading::TaskManager *manager, core::u32 initialValue,core::u32 fiberSlots) : manager(manager),counter(initialValue) , freeSlots(freeSlotsStorage) , freeSlotsStorage() , waitingFibers(waitingFiberStorage) , fiberSlots(fiberSlots)
+{
+    if(fiberSlots  > NUMBER_OF_WAITING_FIBERS)
+    {
+        freeSlots = new std::atomic<bool>[fiberSlots];
+        waitingFibers = new WaitingFiberBundle[fiberSlots];
+    }
+
+    for(u32 i = 0;i < fiberSlots; i++)
+    {
+        freeSlots[i].store(true);
+        waitingFibers[i].inUse.store(true);
+    }
+
+}
+
+core::threading::BaseCounter::~BaseCounter()
+{
+    while (lock.load(std::memory_order_relaxed) > 0)
+    {
+        std::this_thread::yield();
+    }
+
+    if (fiberSlots > NUMBER_OF_WAITING_FIBERS)
+    {
+        delete[] freeSlots;
+        delete[] waitingFibers;
+    }
+}
+
+bool core::threading::BaseCounter::addFiberToWaitingList(core::vptr fiberBundle, core::u32 targetValue,core::u32 pinnedThreadIndex)
+{
+    for(u32 i = 0;i < fiberSlots; i++)
+    {
+        bool expected{true};
+        if(!std::atomic_compare_exchange_strong_explicit(&freeSlots[i],&expected, false,std::memory_order_seq_cst,std::memory_order_relaxed))
+        {
+            continue;
+        }
+
+        waitingFibers[i].fiberBundle = fiberBundle;
+        waitingFibers[i].targetValue = targetValue;
+        waitingFibers[i].inUse.store(false,std::memory_order_seq_cst);
+
+        auto value = counter.load(std::memory_order_relaxed);
+
+        if (waitingFibers[i].inUse.load(std::memory_order_acquire))
+        {
+            return false;
+        }
+
+        if(waitingFibers[i].targetValue == value)
+        {
+            expected = false;
+
+            if(!std::atomic_compare_exchange_strong_explicit(&waitingFibers[i].inUse,&expected, true,std::memory_order_seq_cst,std::memory_order_relaxed))
+            {
+                return false;
+            }
+
+            freeSlots[i].store(true,std::memory_order_release);
+
+            return true;
+
+        }
+
+
+    }
+
+    HEXEN_ASSERT(false,"All the waiting fiber slots are full. Not able to add another wait.\n"
+               "Increase the value of fiberSlots in the constructor or modify your algorithm to use less waits on the same counter");
+    return false;
+}
+
+void core::threading::BaseCounter::checkWaitingFibers(core::u32 value)
+{
+    for(u32 i = 0;i < fiberSlots;i++)
+    {
+        if(freeSlots[i].load(std::memory_order_acquire))
+        {
+            continue;
+        }
+
+        if(waitingFibers[i].inUse.load(std::memory_order_acquire))
+        {
+            continue;
+        }
+
+        if(waitingFibers[i].targetValue == value)
+        {
+            bool expected = false;
+
+            if(!std::atomic_compare_exchange_strong_explicit(&waitingFibers[i].inUse,&expected, true,std::memory_order_seq_cst,std::memory_order_relaxed))
+            {
+                continue;
+            }
+
+            //manager->addReadyFiber(waitingFibers[i].pinnedThreadIndex, reinterpret_cast<TaskManager::ReadyFiberBundle *>(waitingFibers[i].fiberBundle));
+
+            freeSlots[i].store(true,std::memory_order_release);
+
+            return true;
+
+        }
+
+    }
+}
+
+core::threading::BaseCounter::WaitingFiberBundle::WaitingFiberBundle() : inUse(true) , pinnedThreadIndex(std::numeric_limits<u32>::max())
+{
+}
---
+++ engine/src/core/threading/BaseCounter.h
@@ -0,0 +1,60 @@
+//
+// Created by cx9ps3 on 14.07.2023.
+//
+
+#pragma once
+
+#include <atomic>
+#include "../Types.h"
+
+namespace core::threading
+{
+    class TaskManager;
+    class BaseCounter
+    {
+    protected:
+
+        static constexpr const u8 NUMBER_OF_WAITING_FIBERS = 4;
+        TaskManager *manager;
+        std::atomic<u32> counter{0};
+        std::atomic<u32> lock{0};
+
+        std::atomic<bool> *freeSlots{nullptr};
+        std::atomic<bool> freeSlotsStorage[NUMBER_OF_WAITING_FIBERS];
+
+        struct WaitingFiberBundle
+        {
+            WaitingFiberBundle();
+
+            std::atomic<bool> inUse;
+
+            vptr fiberBundle{nullptr};
+
+            u32 targetValue{0};
+
+            u32 pinnedThreadIndex;
+
+        };
+
+        WaitingFiberBundle *waitingFibers{nullptr};
+        WaitingFiberBundle waitingFiberStorage[NUMBER_OF_WAITING_FIBERS];
+
+        u32 fiberSlots{0};
+
+        friend  class TaskManager;
+
+        bool addFiberToWaitingList(vptr fiberBundle,u32 targetValue, u32 pinnedThreadIndex = std::numeric_limits<u32>::max());
+
+        void checkWaitingFibers(u32 value);
+
+    public:
+       explicit BaseCounter(TaskManager* manager,u32 initialValue = 0, u32 fiberSlots = NUMBER_OF_WAITING_FIBERS);
+
+        BaseCounter(BaseCounter const &) = delete;
+        BaseCounter(BaseCounter &&) noexcept = delete;
+        BaseCounter &operator=(BaseCounter const &) = delete;
+        BaseCounter &operator=(BaseCounter &&) noexcept = delete;
+        ~BaseCounter();
+
+    };
+}
