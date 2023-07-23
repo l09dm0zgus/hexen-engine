45609d7040cf9d8504828bd33c52400743a298e9
l09dm0zgus <cx9ps3@gmail.com>
2023-07-24 01:41:46 +0300

    added EventCallbacks

---
+++ engine/src/core/threading/Callbacks.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../Types.h"
+namespace core::threads
+{
+
+using ThreadCreationCallback = void (*)(void *context, unsigned threadCount);
+
+using FiberCreationCallback = void (*)(void *context, unsigned fiberCount);
+
+using ThreadEventCallback = void (*)(void *context, unsigned threadIndex);
+
+using FiberAttachedCallback = void (*)(void *context, unsigned fiberIndex);
+
+using FiberDetachedCallback = void (*)(void *context, unsigned fiberIndex, bool isMidTask);
+
+struct EventCallbacks
+{
+	void *context = nullptr;
+
+	ThreadCreationCallback onThreadsCreated = nullptr;
+	FiberCreationCallback onFibersCreated = nullptr;
+
+	ThreadEventCallback onWorkerThreadStarted = nullptr;
+	ThreadEventCallback onWorkerThreadEnded = nullptr;
+
+	FiberAttachedCallback onFiberAttached = nullptr;
+	FiberDetachedCallback onFiberDetached = nullptr;
+};
+}
+
