65ca249cc77d86443b483d49d7d3068fe41f6f4a
l09dm0zgus <cx9ps3@gmail.com>
2023-07-29 16:51:56 +0300

    Add new Threading.h to core/threading directory
    This commit adds a new file, Threading.h, to the core/threading directory in the engine. Threading.h includes all the necessary headers required for multi-threading in the engine, improving organisation, making the code more readable and easier to debug.

---
+++ engine/src/core/threading/Threading.h
@@ -0,0 +1,16 @@
+//
+// Created by cx9ps3 on 29.07.2023.
+//
+#pragma once
+
+#include "counter/AtomicCounter.h"
+#include "counter/TaskCounter.h"
+#include "counter/BaseCounter.h"
+#include "task/Callbacks.h"
+#include "task/Task.h"
+#include "task/TaskScheduler.h"
+#include "threads/Config.h"
+#include "threads/Fiber.h"
+#include "threads/Thread.h"
+#include "threads/WaitFreeQueue.h"
+
