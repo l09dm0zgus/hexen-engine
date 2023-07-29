522b0b7ef87c50ae279e32a80ad55dd41b9d45c2
l09dm0zgus <cx9ps3@gmail.com>
2023-07-29 23:11:50 +0300

    Refactor GameLoop class and extend functionality
    Refactored GameLoop class, expanding its API and improving code readability with comments. Removed inheritance from MemoryPool and replaced it with AllocatedObject. Converted several methods to virtual for flexibility and added an initializer for TaskSystem and SystemManager. This functionality in the game loop allows us to handle the main game loop, controlling game updates and rendering processes with frame rate control.

--- engine/src/core/application/GameLoop.cpp
+++ engine/src/core/application/GameLoop.cpp
@@ -5,40 +5,49 @@
 #include "GameLoop.h"
 #include <chrono>
 #include "../window/Window.h"
+#include "../../systems/TaskSystem.h"
+#include "../../systems/RenderSystem.h"
 
 void core::GameLoop::start()
 {
-
+    sys::SystemManager::addRenderSystem<sys::RenderSystem>(100);
+    systemManager->start();
 }
 
 void core::GameLoop::loop(const std::shared_ptr<Window> &window)
 {
     initializeClock();
+
     while (window->isOpen())
     {
         setFrameStart();
-        //process input
-
         setAccumulator();
+
+        //process input
+        systemManager->processInput(window);
 
         while (accumulator > deltaTime)
         {
             //update
+            systemManager->update(deltaTime);
             accumulator -= deltaTime;
         }
 
         //render
-
-
-
+        systemManager->render(getAlpha());
+        sys::TaskSystem::waitForCounter();
     }
 }
+
+
 
 void core::GameLoop::initializeClock()
 {
     framesStart = std::chrono::steady_clock::now();
 
 }
+
+
 
 void core::GameLoop::setFrameStart()
 {
@@ -49,6 +58,8 @@ void core::GameLoop::setFrameStart()
     framesStart = currentTime;
 
 }
+
+
 
 void core::GameLoop::setAccumulator()
 {
@@ -61,4 +72,21 @@ void core::GameLoop::setAccumulator()
 float core::GameLoop::getAlpha()
 {
     return accumulator / deltaTime;
+}
+
+core::GameLoop::GameLoop() : mem::AllocatedObject()
+{
+    //initialize thread and fiber pool
+    sys::TaskSystem::initialize();
+
+    systemManager = mem::make_shared<sys::SystemManager>();
+
+    HEXEN_ASSERT(systemManager != nullptr , "System manager is nullptr!");
+
+    sys::SystemManager::setCurrentSystemManager(systemManager.get());
+}
+
+core::GameLoop::~GameLoop()
+{
+    sys::SystemManager::setCurrentSystemManager(nullptr);
 }
