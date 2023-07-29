5345129026010ae04ed3bbf1457608be3434f711
l09dm0zgus <cx9ps3@gmail.com>
2023-07-29 22:10:25 +0300

    Refactor SystemManager to handle various systems
    The SystemManager has been overhauled to integrate TaskSystem for scheduled tasks execution, graphics, and threading functionalities. This refactoring allows the SystemManager to process inputs, track and manage multiple rendering and gameplay systems, and maintain count of components in the game engine. The enhancements enhance task and memory management, making the game engine more versatile and efficient.

--- engine/src/systems/SystemManager.cpp
+++ engine/src/systems/SystemManager.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "SystemManager.h"
+#include "../core/graphics/Graphics.h"
+#include "../core/window/Window.h"
+#include "TaskSystem.h"
+#include <memory>
+
+void sys::SystemManager::processInput(const std::shared_ptr<core::Window> &window)
+{
+    //in future will be created InputSystem for handling input
+    SDL_Event event;
+    while (window->isOpen())
+    {
+        while (window->pollEvents(&event))
+        {
+            //editorGui.processEvent(&event);
+            if (event.type == SDL_EVENT_QUIT || event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
+            {
+                window->close();
+            }
+            else if (event.type == SDL_EVENT_WINDOW_RESIZED)
+            {
+               window->resize();
+            }
+        }
+        window->pollEvents(&event);
+    }
+}
+
+void sys::SystemManager::start()
+{
+    for(auto& system : renderSystems)
+    {
+        TaskSystem::addTask(core::threading::TaskPriority::High,system.get(),&IRenderSystem::start);
+    }
+
+    for(auto& system : gameplaySystems)
+    {
+        TaskSystem::addTask(core::threading::TaskPriority::High,system.get(),&IGamePlaySystem::start);
+    }
+
+}
+
+void sys::SystemManager::render(float alpha)
+{
+    for(auto& system : renderSystems)
+    {
+        TaskSystem::addTask(core::threading::TaskPriority::High,system.get(),&IRenderSystem::render,alpha);
+    }
+}
+
+void sys::SystemManager::update(float deltaTime)
+{
+    for(auto& system : gameplaySystems)
+    {
+        TaskSystem::addTask(core::threading::TaskPriority::High,system.get(),&IGamePlaySystem::update,deltaTime);
+    }
+}
