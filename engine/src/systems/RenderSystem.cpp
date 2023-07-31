2a73da4bccd7418b596b05312eb94500c79eea07
l09dm0zgus <cx9ps3@gmail.com>
2023-07-31 19:59:49 +0300

    Add extensive commenting to RenderSystem for clarity
    Added comprehensive commenting and documentation in `RenderSystem.h` and `RenderSystem.cpp` to explain the functions, variables, and general functionality of the RenderSystem class. This will enhance code readability and maintainability. Similarly, some functions were updated to use TaskSystem for parallel execution, which improves overall system performance. Furthermore, the primary code change introduced a new strategy for handling the main camera in a scene by using an ID reference rather than looping over all available cameras. This change could offer more versatility and improved performance in the rendering process.

--- engine/src/systems/RenderSystem.cpp
+++ engine/src/systems/RenderSystem.cpp
@@ -4,11 +4,19 @@
 
 #include "RenderSystem.h"
 #include <algorithm>
+#include "TaskSystem.h"
+
 
 std::vector<comp::rend::SpriteComponent> sys::RenderSystem::spritesComponent;
+
 std::vector<comp::rend::SpriteInstancedComponent> sys::RenderSystem::instancedSpritesComponents;
+
 std::vector<comp::TransformComponent> sys::RenderSystem::transformComponents;
-std::vector<comp::CameraComponent> sys::RenderSystem::camerasComponents;
+
+std::vector<std::shared_ptr<comp::CameraComponent>> sys::RenderSystem::camerasComponents;
+
+core::i32 sys::RenderSystem::mainCameraId{0};
+
 
 sys::RenderSystem::RenderSystem(core::u32 sizeOfVectors)
 {
@@ -18,15 +26,15 @@ sys::RenderSystem::RenderSystem(core::u32 sizeOfVectors)
     camerasComponents.reserve(sizeOfVectors);
 }
 
-std::shared_ptr<comp::rend::SpriteComponent>
-sys::RenderSystem::createSpriteComponent(const std::string &vertexShaderPath,const std::string &fragmentShaderPath)
+std::shared_ptr<comp::rend::SpriteComponent> sys::RenderSystem::createSpriteComponent(const std::string &vertexShaderPath, const std::string &fragmentShaderPath)
 {
     spritesComponent.emplace_back(vertexShaderPath,fragmentShaderPath);
     return std::shared_ptr<comp::rend::SpriteComponent>(&*spritesComponent.end());
 }
 
-std::shared_ptr<comp::rend::SpriteInstancedComponent>
-sys::RenderSystem::createSpriteInstancedSprite(const std::string &vertexShaderPath,const std::string &fragmentShaderPath,core::i32 numberOfInstances,glm::mat4 *instancesMatrices)
+
+
+std::shared_ptr<comp::rend::SpriteInstancedComponent> sys::RenderSystem::createSpriteInstancedSprite(const std::string &vertexShaderPath, const std::string &fragmentShaderPath, core::i32 numberOfInstances, glm::mat4 *instancesMatrices)
 {
     instancedSpritesComponents.emplace_back(vertexShaderPath,fragmentShaderPath,numberOfInstances,instancesMatrices);
     return std::shared_ptr<comp::rend::SpriteInstancedComponent>(&*instancedSpritesComponents.end());
@@ -38,61 +46,78 @@ std::shared_ptr<comp::TransformComponent> sys::RenderSystem::createTransformComp
     return std::shared_ptr<comp::TransformComponent>(&*transformComponents.end());
 }
 
-std::shared_ptr<comp::CameraComponent>
-sys::RenderSystem::createCameraComponent(core::i32 viewportWidth, core::i32 viewportHeight,float FOV)
-{
-    camerasComponents.emplace_back(viewportWidth,viewportHeight,FOV);
-    return std::shared_ptr<comp::CameraComponent>(&*camerasComponents.end());
-}
 
-void
-sys::RenderSystem::updateSpriteModelMatrix(comp::rend::SpriteComponent &spriteComponent)
+
+void sys::RenderSystem::updateSpriteModelMatrix(comp::rend::SpriteComponent *spriteComponent)
 {
     auto iter = std::find_if(transformComponents.begin(), transformComponents.end(), [sprite = spriteComponent](auto &transform){
-        return transform.isDirty() && transform.getOwnerUUID() == sprite.getOwnerUUID();
+        return transform.isDirty() && transform.getOwnerUUID() == sprite->getOwnerUUID();
     });
     if(iter != transformComponents.end())
     {
-        spriteComponent.setTransformMatrix(iter->getTransformMatrix());
+        spriteComponent->setTransformMatrix(iter->getTransformMatrix());
     }
 }
 
-void
-sys::RenderSystem::updateViewAndProjectionMatrices(comp::rend::SpriteComponent *spriteComponent)
+
+void sys::RenderSystem::updateViewAndProjectionMatrices(comp::rend::SpriteComponent *spriteComponent)
 {
-    for(auto& camera:camerasComponents )
-    {
-        spriteComponent->setProjectionMatrix(camera.getProjectionMatrix());
-        spriteComponent->setViewMatrix(camera.getViewMatrix());
-    }
+    spriteComponent->setProjectionMatrix(camerasComponents[mainCameraId]->getProjectionMatrix());
+    spriteComponent->setViewMatrix(camerasComponents[mainCameraId]->getViewMatrix());
 }
 
 void sys::RenderSystem::start()
 {
     for(auto& sprite:spritesComponent)
     {
-        updateSpriteModelMatrix(sprite);
-        updateViewAndProjectionMatrices(&sprite);
-        sprite.start();
+        TaskSystem::addTask(core::threading::TaskPriority::Normal, this,&RenderSystem::updateSpriteModelMatrix,&sprite);
+        TaskSystem::addTask(core::threading::TaskPriority::Normal, this,&RenderSystem::updateViewAndProjectionMatrices,&sprite);
+        TaskSystem::addTask<comp::rend::RenderComponent>(core::threading::TaskPriority::Normal, &sprite,&comp::rend::SpriteComponent::start);
+
     }
-    for(auto& spriteInstenced:instancedSpritesComponents)
+    for(auto& spriteInstanced:instancedSpritesComponents)
     {
-        updateViewAndProjectionMatrices(&spriteInstenced);
-        spriteInstenced.start();
+        TaskSystem::addTask<RenderSystem,void,comp::rend::SpriteComponent*>(core::threading::TaskPriority::Normal,this,&RenderSystem::updateViewAndProjectionMatrices,&spriteInstanced);
+        TaskSystem::addTask<comp::rend::RenderComponent>(core::threading::TaskPriority::Normal, &spriteInstanced,&comp::rend::SpriteInstancedComponent::start);
     }
 }
+
+
+
 
 void sys::RenderSystem::render(float alpha)
 {
     for(auto& sprite:spritesComponent)
     {
-        updateSpriteModelMatrix(sprite);
-        updateViewAndProjectionMatrices(&sprite);
-        sprite.update(alpha);
+        TaskSystem::addTask(core::threading::TaskPriority::Normal, this,&RenderSystem::updateSpriteModelMatrix,&sprite);
+        TaskSystem::addTask(core::threading::TaskPriority::Normal, this,&RenderSystem::updateViewAndProjectionMatrices,&sprite);
+        TaskSystem::addTask<comp::rend::RenderComponent>(core::threading::TaskPriority::Normal, &sprite,&comp::rend::SpriteComponent::update,alpha);
     }
-    for(auto& spriteInstenced:instancedSpritesComponents)
+    for(auto& spriteInstanced:instancedSpritesComponents)
     {
-        updateViewAndProjectionMatrices(&spriteInstenced);
-        spriteInstenced.update(alpha);
+        TaskSystem::addTask<RenderSystem,void,comp::rend::SpriteComponent*>(core::threading::TaskPriority::Normal,this,&RenderSystem::updateViewAndProjectionMatrices,&spriteInstanced);
+        TaskSystem::addTask<comp::rend::RenderComponent>(core::threading::TaskPriority::Normal, &spriteInstanced,&comp::rend::SpriteInstancedComponent::update,alpha);
     }
 }
+
+
+
+
+std::shared_ptr<comp::CameraComponent> sys::RenderSystem::getMainCamera()
+{
+    HEXEN_ASSERT(mainCameraId < camerasComponents.size() , "Failed to get camera.Wrong main camera id!\n");
+    if(!camerasComponents.empty())
+    {
+        return camerasComponents[mainCameraId];
+    }
+
+    return nullptr;
+}
+
+
+template<typename T, std::enable_if_t<std::is_base_of_v<comp::CameraComponent, T>, bool>> void sys::RenderSystem::addCameraComponent(core::i32 viewportWidth, core::i32 viewportHeight,float FOV)
+{
+    camerasComponents.emplace_back(core::mem::make_shared<T>(viewportWidth,viewportHeight,FOV));
+}
+
+
--- engine/src/systems/RenderSystem.h
+++ engine/src/systems/RenderSystem.h
@@ -18,19 +18,226 @@ namespace sys
     class RenderSystem : public IRenderSystem
     {
     public:
+
+        /**
+        * @class RenderSystem
+        * @brief The RenderSystem class handles the rendering of graphics.
+        *
+        *  The RenderSystem class manages the rendering process by creating and
+        * managing various rendering resources. It provides functions to initialize, update, and render
+        * the graphics in a given scene.
+        */
+
         explicit RenderSystem(core::u32 sizeOfVectors);
+
+        /**
+        * @brief Creates a sprite component.
+        *
+        * This function creates a sprite component using the provided vertex shader and fragment shader paths.
+        * The vertexShaderPath parameter specifies the path to the vertex shader file, and
+        * the fragmentShaderPath parameter specifies the path to the fragment shader file.
+        *
+        * @param vertexShaderPath The path to the vertex shader file.
+        * @param fragmentShaderPath The path to the fragment shader file.
+        * @return A sprite component object.
+        *
+        * @warning The vertexShaderPath and fragmentShaderPath parameters must be valid and accessible paths.
+        *
+        */
+
+
         static std::shared_ptr<comp::rend::SpriteComponent> createSpriteComponent(const std::string &vertexShaderPath , const std::string &fragmentShaderPath);
+
+        /**
+        * Creates an instanced sprite with the provided vertex and fragment shader paths,
+        * for the given number of instances and instances matrices.
+        *
+        * @param vertexShaderPath The path to the vertex shader source file.
+        * @param fragmentShaderPath The path to the fragment shader source file.
+        * @param numberOfInstances The number of instances to be rendered.
+        * @param instancesMatrices An array of matrices representing the position and orientation
+        *                         of each instance.
+        * @return A handle to the created instanced sprite.
+        *
+        * @note The provided shader paths must be valid.
+        * @note The provided number of instances should be positive.
+        * @note The size of the instancesMatrices array should match the provided number of instances.
+        *       Each element in the array represents the transformation matrix for an instance.
+        *       The matrix should be in the column-major order.
+        */
+
         static std::shared_ptr<comp::rend::SpriteInstancedComponent> createSpriteInstancedSprite(const std::string &vertexShaderPath , const std::string &fragmentShaderPath,core::i32 numberOfInstances,glm::mat4 *instancesMatrices);
+
+        /**
+        * @brief Creates a transform component for rendering purposes.
+        *
+        * This function creates and returns a transform component object that can be used in rendering systems.
+        * The transform component stores the position, rotation, and scale of an entity in 3D space.
+        *
+        * @return A pointer to the newly created transform component.
+        */
+
         static std::shared_ptr<comp::TransformComponent> createTransformComponent();
-        static std::shared_ptr<comp::CameraComponent> createCameraComponent(core::i32 viewportWidth, core::i32 viewportHeight, float FOV);
+
+
+
+        /**
+         * @brief Adds a camera component to the render system.
+         *
+         * This function adds a camera component to the render system, allowing rendering of objects
+         * from the viewpoint of the camera. The camera component is associated with a specific viewport
+         * size and field of view (FOV).
+         *
+         * @param viewportWidth The width of the viewport in pixels.
+         * @param viewportHeight The height of the viewport in pixels.
+         * @param FOV The field of view (FOV) for the camera, specified in degrees.
+         *            The FOV determines the extent of the scene visible through the camera.
+         *            A smaller FOV provides a zoomed-in view, while a larger FOV provides a wider view.
+         *
+         * @details
+         * This function creates and initializes a new camera component with the specified viewport
+         */
+
+        template<typename T, std::enable_if_t<std::is_base_of_v<comp::CameraComponent, T>, bool> = true>
+        static void addCameraComponent(core::i32 viewportWidth, core::i32 viewportHeight, float FOV);
+
+        /**
+        * @brief Retrieves the main camera instance from the RenderSystem.
+        *
+        * This function returns the main camera object managed by the RenderSystem. The main camera is responsible for rendering
+        * the scene from the perspective of the viewer. It is typically used to set up the view and projection matrices,
+        * control the camera position and orientation, and perform various camera-related operations.
+        *
+        * @note The main camera must be set before rendering the scene.
+        *
+        * @return The main camera instance.
+        *
+        */
+
+        static std::shared_ptr<comp::CameraComponent> getMainCamera();
+
+        /**
+        * @brief Starts the rendering system
+        *
+        * This function initializes the rendering system and prepares it for rendering
+        * operations. It should be called before any rendering is done.
+        *
+        * @par Example:
+        * @code
+        * sys::RenderSystem renderer;
+        * renderer.start();
+        * @endcode
+        */
+
         void start() override;
+
+        /**
+        * @brief Render the scene with the given alpha value.
+        *
+        * This function is a member of the RenderSystem class within the sys namespace. It is responsible
+        * for rendering the scene with the provided alpha value.
+        *
+        * @param alpha interpolated va;ue for correct rendering.
+        *
+        * @see sys::RenderSystem
+        */
+
         void render(float alpha) override;
     private:
+
+        /**
+        * @brief Represents the spritesComponent of the RenderSystem in the sys namespace.
+        *
+        * The spritesComponent is responsible for managing the sprites of the RenderSystem.
+        * It provides methods to add, remove, and update the sprites in the system.
+        *
+        */
+
         static std::vector<comp::rend::SpriteComponent> spritesComponent;
+
+        /**
+        * @brief The instancedSpritesComponents variable stores the sprite components
+        *        used for instanced rendering in the RenderSystem.
+        *
+        * This variable is accessed by the RenderSystem class to perform instanced
+        * rendering of sprites. It should be populated with sprite components of
+        * entities that require instanced rendering.
+        *
+        **/
+
+
         static std::vector<comp::rend::SpriteInstancedComponent> instancedSpritesComponents;
+
+        /**
+        * @brief Represents a container for render system transform components.
+        *
+        * The transformComponents  represents a container that holds the transform
+        * components of the render system. It is used within the sys namespace and belongs
+        * to the RenderSystem subsystem.
+        *
+        * This container is used to store and manage the transform components associated
+        * with the entities in the render system. It allows for easy access and manipulation
+        * of the transform data required for rendering.
+        *
+        */
+
         static std::vector<comp::TransformComponent> transformComponents;
-        static std::vector<comp::CameraComponent> camerasComponents;
-        void updateSpriteModelMatrix(comp::rend::SpriteComponent &spriteComponent);
+
+        /**
+        * @brief The camerasComponents variable is a member of the sys::RenderSystem class.
+        *
+        * This variable holds the camera components used by the render system. It is an
+        * internal storage for managing render-related components.
+        *
+        * @see sys::RenderSystem
+        * @see CameraComponent
+        */
+
+        static std::vector<std::shared_ptr<comp::CameraComponent>> camerasComponents;
+
+        /**
+        * @brief The identifier of the main camera in the Render System.
+        *
+        * The mainCameraId variable represents the identifier assigned to the main camera within the Render System.
+        * This identifier is used to reference the main camera in various operations and settings related to rendering.
+        *
+        * By default, the mainCameraId is set to 0.
+        *
+        * It is important to note that the mainCameraId can be changed if needed. However, altering this value without
+        * proper understanding of the Render System's functionality may lead to undesirable results or unexpected
+        * behavior in the rendering process.
+        *
+        * @see sys::RenderSystem
+        */
+
+        static core::i32 mainCameraId;
+
+        /**
+        * @brief Updates the model matrix of the sprite component.
+        *
+        * This function calculates and updates the model matrix of the specified sprite component.
+        * The model matrix is used to transform the sprite component's vertices from object-space to world-space.
+        *
+        * @param spriteComponent A pointer to the sprite component to update.
+        */
+
+
+        void updateSpriteModelMatrix(comp::rend::SpriteComponent *spriteComponent);
+
+        /**
+        * @brief Updates the view and projection matrices for rendering a sprite component.
+        *
+        * This function is used to update the view and projection matrices required for rendering
+        * a sprite component on the screen. It takes a pointer to a sprite component as its parameter
+        * and calculates the appropriate matrices based on the sprite's position, rotation, and scale.
+        * The updated matrices can then be used in the rendering process.
+        *
+        * @param spriteComponent A pointer to the sprite component whose view and projection matrices
+        *                        are to be updated.
+        *
+        * @see comp::rend::SpriteComponent
+        */
+
         void updateViewAndProjectionMatrices(comp::rend::SpriteComponent *spriteComponent);
     };
 }
