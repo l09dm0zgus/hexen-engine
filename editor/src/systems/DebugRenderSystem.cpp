185badfaace5806ab31948baece55a1cb6b822ae
l09dm0zgus <cx9ps3@gmail.com>
2023-07-31 21:13:34 +0300

    Implement debug rendering system enhancements and improvements
    The DebugRenderSystem has been significantly expanded and improved. Previously, the system was minimal, and lacked necessary features for extensive testing scenarios. Now, it inherits from ::sys::IRenderSystem, allowing it to be included in the main rendering routine. Numerous new functionalities have been added, such as an addDebugGrid function, and updateModelMatrix, updateViewAndProjectionMatrices for better control on render components. All new methods have robust docstring to facilitate developers' understanding of the codebase. The hope is that these improvements will make the debug rendering system more beneficial in identifying, tracing, and solving issues during development.

--- editor/src/systems/DebugRenderSystem.cpp
+++ editor/src/systems/DebugRenderSystem.cpp
@@ -2,19 +2,50 @@
 // Created by cx9ps3 on 13.07.2023.
 //
 
+#include <systems/TaskSystem.h>
+#include <systems/RenderSystem.h>
 #include "DebugRenderSystem.h"
 
-sys::DebugRenderSystem::DebugRenderSystem(core::u32 sizeOfVectors)
+edit::sys::DebugRenderSystem::DebugRenderSystem(core::u32 sizeOfVectors)
 {
 
 }
 
-void sys::DebugRenderSystem::start()
+void edit::sys::DebugRenderSystem::start()
 {
 
 }
 
-void sys::DebugRenderSystem::update(float deltaTime)
+void edit::sys::DebugRenderSystem::render(float alpha)
 {
-
+    if (debugGridComponent != nullptr)
+    {
+        ::sys::TaskSystem::addTask<DebugRenderSystem,void,::comp::rend::RenderComponent*>(core::threading::TaskPriority::Normal, this,&DebugRenderSystem::updateModelMatrix,debugGridComponent.get());
+        ::sys::TaskSystem::addTask<DebugRenderSystem,void,::comp::rend::RenderComponent*>(core::threading::TaskPriority::Normal, this,&DebugRenderSystem::updateViewAndProjectionMatrices,debugGridComponent.get());;
+        ::sys::TaskSystem::addTask(core::threading::TaskPriority::Normal,debugGridComponent.get(),&comp::rend::DebugGridComponent::draw);
+    }
 }
+
+void edit::sys::DebugRenderSystem::addDebugGrid()
+{
+    if(debugGridComponent != nullptr)
+    {
+        debugGridComponent = core::mem::make_shared<comp::rend::DebugGridComponent>("vertex.glsl","debugFragment.glsl");
+        debugGridTransform = core::mem::make_shared<::comp::TransformComponent>(glm::vec2{0.0f});
+    }
+}
+
+void edit::sys::DebugRenderSystem::updateModelMatrix(::comp::rend::RenderComponent *renderComponent)
+{
+    renderComponent->setTransformMatrix(debugGridTransform->getTransformMatrix());
+}
+
+void edit::sys::DebugRenderSystem::updateViewAndProjectionMatrices(::comp::rend::RenderComponent *renderComponent)
+{
+    auto camera = ::sys::RenderSystem::getMainCamera();
+    HEXEN_ASSERT(camera != nullptr , "Main camera is nullptr!\n");
+
+    renderComponent->setProjectionMatrix(camera->getProjectionMatrix());
+    renderComponent->setViewMatrix(camera->getViewMatrix());
+}
+
--- editor/src/systems/DebugRenderSystem.h
+++ editor/src/systems/DebugRenderSystem.h
@@ -2,24 +2,113 @@
 // Created by cx9ps3 on 13.07.2023.
 //
 
-#ifndef HEXENEDITOR_DEBUGRENDERSYSTEM_H
-#define HEXENEDITOR_DEBUGRENDERSYSTEM_H
+#pragma once
 
 #include <core/Types.h>
-
-namespace sys
+#include <systems/IRenderSystem.h>
+#include "../components/debug_rendering/DebugGridComponent.h"
+namespace comp
 {
-    class DebugRenderSystem
-    {
-        explicit DebugRenderSystem(core::u32 sizeOfVectors);
-        void start();
-        void update(float deltaTime);
+    class TransformComponent;
+}
+
+namespace edit::sys
+{
+class DebugRenderSystem : public ::sys::IRenderSystem
+{
+    public:
+
+    /**
+    * @class DebugRenderSystem
+    * @brief A system for rendering debug information.
+    *
+    * This class provides functionalities to render debug information in a editor.
+    *
+    * @details
+    * The DebugRenderSystem class is responsible for rendering debug information such as lines,
+    * shapes, and text onto the screen. It utilizes a fixed-size vector to store the debug data
+    * and provides methods to add, remove, and clear the debug data. Rendering is done by calling
+    * the render method of this class.
+    *
+    */
+
+    explicit DebugRenderSystem(core::u32 sizeOfVectors);
+
+    /**
+    * @brief Start the operation.
+    *
+    * This function overrides the start() method of the base class and is called to initiate the operation.
+    * It performs any necessary initialization and starts the execution of the operation.
+    *
+    * @note This function is meant to be overridden by derived classes. Do not call this function directly.
+    *
+    *
+    * @see BaseClass::start()
+    */
+
+    void start() override;
+
+    /**
+    * @brief Applies linear interpolation to render the object based on the given alpha value.
+    *
+    * This function overrides the base class's render function and applies linear interpolation
+    * using the given alpha value to produce an interpolated render of the object.
+    *
+    * @param alpha The alpha value for linear interpolation (between 0 and 1).
+    *
+    * @note This function is an override function and should be called through the base class's render function.
+    * It interpolates the object's render based on the given alpha value.
+    *
+    */
+
+    void render(float alpha) override;
+
+    /**
+    * @brief Adds a debugging grid to the screen.
+    *
+    * This function adds a debugging grid to the screen by drawing a grid made of lines using the current graphics context.
+    * The grid is useful for visually verifying the positioning and alignment of sprites in the application.
+    *
+    * @note This function does not return any value.
+    *
+    */
+
+    void addDebugGrid();
     private:
-        //void updateSpriteModelMatrix(comp::rend::SpriteComponent &spriteComponent);
-        //void updateViewAndProjectionMatrices(comp::rend::SpriteComponent *spriteComponent);
+
+    /**
+    * @brief Updates the model matrix of the sprite component.
+    *
+    * This function calculates and updates the model matrix of the specified sprite component.
+    *  The model matrix is used to transform the sprite component's vertices from object-space to world-space.
+    *
+    * @param renderComponent A pointer to the sprite component to update.
+     */
+
+
+    void updateModelMatrix(::comp::rend::RenderComponent *renderComponent);
+
+    /**
+    * @brief Updates the view and projection matrices for rendering a sprite component.
+    *
+    * This function is used to update the view and projection matrices required for rendering
+    * a sprite component on the screen. It takes a pointer to a sprite component as its parameter
+    * and calculates the appropriate matrices based on the sprite's position, rotation, and scale.
+    * The updated matrices can then be used in the rendering process.
+    *
+    * @param renderComponent A pointer to the sprite component whose view and projection matrices
+    *                        are to be updated.
+    *
+    * @see comp::rend::RenderComponent
+    */
+
+    void updateViewAndProjectionMatrices(::comp::rend::RenderComponent *renderComponent);
+
+    std::shared_ptr<comp::rend::DebugGridComponent> debugGridComponent{nullptr};
+
+    std::shared_ptr<::comp::TransformComponent> debugGridTransform;
     };
 }
 
 
 
-#endif //HEXENEDITOR_DEBUGRENDERSYSTEM_H
