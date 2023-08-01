c11f7535633ed1b27f12151315a2ad4ba0531cb5
l09dm0zgus <cx9ps3@gmail.com>
2023-08-01 18:09:41 +0300

    Add EditorSystemsManager for managing editor environment
    Introduced a new EditorSystemsManager class that will allow managing various systems used in an editor environment. This class, built upon SystemsManager, includes methods to create, update, and destroy editor systems. Additionally, it provides an interface (setEditorGUI) to customize Editor GUI object, offers methods to process user inputs, claims control over the start and update process of the system, manages rendering and supports debugging by visualizing the layout and alignment of objects in the scene. This addition will enable more effective management of tools and systems within the application's editor environment.

---
+++ editor/src/systems/EditorSystemsManager.cpp
@@ -0,0 +1,58 @@
+//
+// Created by cx9ps3 on 01.08.2023.
+//
+
+#include "EditorSystemsManager.h"
+#include <core/window/Window.h>
+#include <systems/TaskSystem.h>
+#include "../gui/EditorGUI.h"
+#include "DebugRenderSystem.h"
+
+void edit::sys::EditorSystemsManager::setEditorGUI(const std::shared_ptr<gui::EditorGUI> &newEditorGUI)
+{
+    editorGui  = newEditorGUI;
+}
+
+void edit::sys::EditorSystemsManager::processInput(const std::shared_ptr<core::Window> &window)
+{
+    SDL_Event event;
+    while (window->isOpen())
+    {
+        while (window->pollEvents(&event))
+        {
+            editorGui->processEvent(&event);
+            if (event.type == SDL_EVENT_QUIT || event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
+            {
+                window->close();
+            }
+            else if (event.type == SDL_EVENT_WINDOW_RESIZED)
+            {
+                window->resize();
+            }
+        }
+        window->pollEvents(&event);
+    }
+}
+
+void edit::sys::EditorSystemsManager::start()
+{
+    SystemsManager::start();
+    ::sys::TaskSystem::addTask(::core::threading::TaskPriority::High,debugRenderSystem.get(),&DebugRenderSystem::start);
+}
+
+void edit::sys::EditorSystemsManager::render(float alpha)
+{
+    SystemsManager::render(alpha);
+    ::sys::TaskSystem::addTask(::core::threading::TaskPriority::High,debugRenderSystem.get(),&DebugRenderSystem::render,alpha);
+
+}
+
+void edit::sys::EditorSystemsManager::update(float deltaTime)
+{
+    SystemsManager::update(deltaTime);
+}
+
+void edit::sys::EditorSystemsManager::addDebugGrid()
+{
+    debugRenderSystem->addDebugGrid();
+}
---
+++ editor/src/systems/EditorSystemsManager.h
@@ -0,0 +1,133 @@
+//
+// Created by cx9ps3 on 01.08.2023.
+//
+
+#pragma once
+#include <systems/SystemsManager.h>
+
+namespace edit::gui
+{
+    class EditorGUI;
+}
+
+namespace edit::sys
+{
+    class DebugRenderSystem;
+    class EditorSystemsManager : public ::sys::SystemsManager
+    {
+    private:
+
+        /**
+        * @brief The EditorGui class represents a graphical user interface for an editor.
+        *
+        * This class provides methods to create and manage the editor GUI elements.
+        * It allows users to edit and manipulate text, apply formatting, and perform various actions on the text.
+        *
+        *
+        * @note The EditorGui class is designed to be used as a part of a larger application, and can be customized
+        * and extended to meet specific requirements.
+        */
+
+        std::shared_ptr<gui::EditorGUI> editorGui;
+
+        /**
+        * @file DebugRenderSystem.h
+        *
+        * @brief This file contains the declaration of the DebugRenderSystem class, which
+        *        is responsible for rendering debug information in a game or application.
+        */
+
+        std::shared_ptr<DebugRenderSystem> debugRenderSystem;
+    public:
+
+        /**
+        * @class EditorSystemsManager
+        * @brief The EditorSystemsManager class represents a manager for editor systems.
+        *
+        * This class is responsible for managing various systems used in an editor environment.
+        * It provides functionality for creating, updating, and destroying editor systems.
+        */
+
+        ~EditorSystemsManager() override = default;
+
+        /**
+        * @brief Set the Editor GUI object.
+        *
+        * This function allows setting a new Editor GUI object for the software.
+        *
+        * @param newEditorGUI A shared pointer to the new Editor GUI object to be set.
+        */
+
+        void setEditorGUI(const std::shared_ptr<gui::EditorGUI> &newEditorGUI);
+
+        /**
+        * @brief Process the input received from the window.
+        *
+        * This function is responsible for processing user input received from the window. It overrides a base class function.
+        *
+        * @param window A shared pointer to the core::Window object that received the input.
+        */
+
+        void processInput(const std::shared_ptr<core::Window> &window) override;
+
+        /**
+        * @brief Starts the process.
+        *
+        * This function is an override of the start() method and is responsible for starting the process.
+        * It should be implemented by the derived class.
+        *
+        */
+
+        void start() override;
+
+        /**
+        * @brief Updates the object based on the elapsed time since the last update.
+        *
+        * This function is called in each frame to update the object's state.
+        * It takes into account the elapsed time (in seconds) since the last frame,
+        * allowing for smooth and consistent animations and movements.
+        *
+        * @param deltaTime The elapsed time (in seconds) since the last update frame.
+        *                  This value is typically obtained from a timer or system clock.
+        *                  It represents the elapsed time between the previous and current frame.
+        *                  A smaller deltaTime value means a more frequent update.
+        *                  A larger deltaTime value means a less frequent update.
+        *                  A deltaTime value of 0 indicates no time has elapsed.
+        *
+        * @details This function should be overridden in derived classes to implement custom update logic.
+        *          The derived class should update the object's properties and behavior based on the deltaTime value.
+        *
+        * @note The update function is typically called from a game loop or an update loop.
+        *       It should be called once per frame to keep the object up-to-date with the game world.
+        *       The derived class should handle any necessary calculations or actions based on the elapsedTime value.
+        *       The derived class can make use of the deltaTime value to adjust the object's movement, animation, or any other time-dependent operations.
+        *        If deltaTime is not needed in the derived class implementation, it can be ignored.
+        *
+        * @see GameLoop
+        */
+
+        void update(float deltaTime) override;
+
+
+        /**
+        * @brief Calling render() in all render systems
+        *
+        * @param alpha interpolated value for correct position of rendered object.
+        */
+
+        void render(float alpha) override;
+
+        /**
+        * @brief Add debug grid overlays to the current scene
+        *
+        * This function adds debug grid overlays to the current scene for debugging purposes.
+        * The grid helps visualize the layout and alignment of various objects in the scene.
+        *
+        * @note This function does not return anything.
+        *
+        */
+
+        void addDebugGrid();
+    };
+}
+
