996a3868f624b588a7e336a67137159b20cf89e9
l09dm0zgus <cx9ps3@gmail.com>
2023-08-18 18:38:40 +0300

    Update thread pause method for ARM and other processors
    Updated the `HEXEN_PAUSE()` macro in `Config.h` for better cross-platform support. Previously, it only supported SSE on MSVC x86, MSVC x64, and Clang/GCC. Now, added implementation for ARM64 processors, and for other environments, we now fallback to `std::this_thread::yield()`. These changes enhance performance and compatibility across different platforms.

--- engine/src/core/threading/threads/Config.h
+++ engine/src/core/threading/threads/Config.h
@@ -40,11 +40,14 @@
 
 //               SSE on MSVC x86                            MSVC x64 has SSE              Clang/GCC define
 #if (defined(_M_IX86_FP) && _M_IX86_FP >= 1) || (defined(_M_AMD64) || defined(_M_X64)) || defined(__SSE__)
-#	include <emmintrin.h>
-#	define HEXEN_PAUSE() _mm_pause()
+    #include <emmintrin.h>
+    #define HEXEN_PAUSE() _mm_pause();
+#elif defined __aarch64__
+    #define HEXEN_PAUSE() asm volatile( "isb" : : );
 #else
-#	define HEXEN_PAUSE()
+    #define HEXEN_PAUSE() std::this_thread::yield();
 #endif
+
 
 #if defined(HEXEN_POSIX_THREADS)
 #	define HEXEN_NOINLINE_POSIX __attribute__((noinline))
