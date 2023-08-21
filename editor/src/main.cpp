242ef765b4dc408b6836210d2f81d19d63d5ea82
l09dm0zgus <cx9ps3@gmail.com>
2023-08-21 21:08:19 +0300

    "Remove unnecessary taskManager and thread testing from main
    The diff removes an extraneous task manager and thread testing from the main.cpp. The usage of taskManager was only for testing and not necessary for the operation of the program. Instead, the main function now correctly runs the 'Application' object, making it more efficient and streamlined."

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -2,54 +2,10 @@
 // Created by cx9ps3 on 24.05.2023.
 //
 #include "application/Application.h"
-
-#include <core/threading/TaskManager.h>
-
-void testTask1(int* x)
-{
-   std::cout << "Task 1 with " << *x << std::endl;
-    (*x)++;
-}
-
-struct TestTask2
-{
-    void execute(int* x)
-    {
-        std::cout << "TestTask2::execute with " << *x << std::endl;
-        (*x)++;
-    }
-
-    void operator()(int* x)
-    {
-        std::cout << "TestTask2::operator() with " << *x << std::endl;
-        (*x)++;
-    }
-};
-
-static core::i32 i{0};
-
 int main()
 {
-    //edit::Application app;
-    //app.run();
-
-    core::threading::TaskManager taskManager;
-
-    if(taskManager.initialize() != core::threading::TaskManager::ReturnCode::Succes)
-    {
-        return -1;
-    }
-
-
-    std::shared_ptr<core::threading::TaskCounter> counter = std::make_shared<core::threading::TaskCounter>(&taskManager);
-
-    taskManager.scheduleTask(core::threading::TaskPriority::Normal,counter,testTask1,&i);
-    taskManager.scheduleTask(core::threading::TaskPriority::Normal,counter,testTask1,&i);
-    taskManager.scheduleTask(core::threading::TaskPriority::Normal,counter,testTask1,&i);
-    taskManager.scheduleTask(core::threading::TaskPriority::Normal,counter,testTask1,&i);
-    taskManager.scheduleTask(core::threading::TaskPriority::Normal,counter,testTask1,&i);
-
-    taskManager.waitForCounter(counter);
+    edit::Application app;
+    app.run();
 
     return 0;
 }
