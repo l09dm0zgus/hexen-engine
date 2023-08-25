867b435bea5491693457ac2d5c3a33d279e96c1d
l09dm0zgus <cx9ps3@gmail.com>
2023-08-25 19:50:36 +0300

    Added dynamic grid and related functionalities
    This commit includes significant enhancements to the core::Grid class. A dynamic, two-dimensional grid has been implemented in the Grid.cpp along with a destructor to manage memory properly. The grid is composed of cells, which are represented by a nested struct within the core::Grid class. For managing interactions with cells, a new method 'checkIfPointInCell' has been added which takes a position as vec2 and returns a shared pointer to the cell containing the point, or nullptr if no such cell is found.
    
    This new feature will help in managing game entities and their interactions spatially, improving efficiency and potential for more complex gameplay mechanisms.

--- engine/src/core/scene/Grid.cpp
+++ engine/src/core/scene/Grid.cpp
@@ -28,5 +28,64 @@ core::Grid::Grid()
         size = currentScene->getSize();
         unitSize = currentScene->getUnitSize();
         numberOfCells = size.x * size.y;
+
+        cells = new Cell*[static_cast<u32>(size.y)];
+
+        for(u32 i = 0; i < size.y; i++)
+        {
+             cells[i] = new Cell[static_cast<u32>(size.x)];
+        }
+
+        for(u32 i = 0; i < size.y; i++)
+        {
+            for(u32 j = 0; j < size.x; j++)
+            {
+                cells[i][j] = Cell(j * unitSize.x, i * unitSize.y, unitSize.x, unitSize.y);
+            }
+        }
     }
+}
+
+core::Grid::~Grid()
+{
+    for(u32 i = 0; i < size.y; i++)
+    {
+        delete[] cells[i];
+    }
+    delete[] cells;
+}
+
+
+std::shared_ptr<core::Grid::Cell> core::Grid::checkIfPointInCell(const glm::vec2 &point)
+{
+    auto previousCell = cells[0][0];
+    u32 i = 0;
+    bool found = false;
+
+    for(; i < size.x;i++)
+    {
+        auto cellSizeX = cells[0][i].size.x;
+        auto cellPositionX = cells[0][i].position.x;
+
+        if(cellPositionX <= point.x && point.x <= cellSizeX + cellPositionX)
+        {
+            found = true;
+            break;
+        }
+    }
+    if(found)
+    {
+        for(u32 j = 0; j < size.y; i++)
+        {
+            auto cellSizeY = cells[j][i].size.y;
+            auto cellPositionY = cells[j][i].position.y;
+
+            if(cellPositionY <= point.y && point.y <= cellSizeY + cellPositionY)
+            {
+                return mem::make_shared<Cell>(cells[j][i]);
+            }
+        }
+    }
+
+    return nullptr;
 }
