c46b6784eb30c2b229dd5b9f6dbf094b4e97e20a
l09dm0zgus <cx9ps3@gmail.com>
2023-09-03 22:48:39 +0300

    Implement scene loading and retrieval in SceneManager
    This commit adds the functionality for loading scenes from a provided file path and fetching the current scene in the SceneManager class. The loadScene method takes a file path as input and loads a scene from the file. The getCurrentScene method returns a shared pointer to the currently loaded scene. This enhances SceneManager's functionality and is crucial for scene management in the game engine.

--- engine/src/core/scene/SceneManager.h
+++ engine/src/core/scene/SceneManager.h
@@ -13,7 +13,26 @@ namespace core
     private:
         static std::shared_ptr<Scene> currentScene;
     public:
+
+        /**
+        * @brief Loads a scene from the specified file path.
+        *
+        * This function loads a scene from a file specified by the given file path.
+        *
+        * @param path The path of the scene file to load.
+        */
+
         static void loadScene(const std::string &path);
+
+        /**
+        * @brief Retrieve the current scene from the SceneManager.
+        *
+        * This method returns a pointer to the current scene that is being managed by the SceneManager.
+        * If no scene is currently set, nullptr is returned.
+        *
+        * @return A pointer to the current scene, or nullptr if no scene is set.
+        */
+
         static std::shared_ptr<Scene> getCurrentScene();
     };
 }
