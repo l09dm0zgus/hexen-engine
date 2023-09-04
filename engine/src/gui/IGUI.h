6a0db49815dd589dd2da231466c21f32f092661c
l09dm0zgus <cx9ps3@gmail.com>
2023-09-05 01:02:30 +0300

    Add IGUI class as a base for GUI objects
    This commit introduces a new Interface IGUI in the GUI subsystem, laying the groundwork for GUI object interactions in the game engine. IGUI includes a virtual method to process SDL events, allowing any object that inherits from this interface to handle GUI-based events. It follows the rule of five, with the move and copy assignments/constructors being explicitly deleted to prevent inappropriate copying or moving, respecting the unique ownership philosophy.

---
+++ engine/src/gui/IGUI.h
@@ -0,0 +1,32 @@
+//
+// Created by cx9ps3 on 03.09.2023.
+//
+
+#pragma once
+
+#include <SDL_events.h>
+#include "../core/memory_pool/AllocatedObject.h"
+
+namespace gui
+{
+    /**
+     * @class IGUI
+     * @brief Interface for managing widgets on screen.
+     *
+     * This interface is used in classes that manage widgets on screen.
+     * Has one virtual method for processing events from SDL.
+     */
+
+    class IGUI : public core::mem::AllocatedObject
+    {
+    public:
+        IGUI() = default;
+        IGUI(IGUI&&) = delete;
+        virtual ~IGUI() = default;
+        IGUI(const IGUI&) = delete;
+        IGUI& operator=(IGUI&&) = delete;
+        IGUI& operator=(const IGUI&) = delete;
+        virtual void processEvent(const SDL_Event &event) = 0;
+    };
+
+}
