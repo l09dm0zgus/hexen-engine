5ab76571e169fc16687464870613e761206a39f9
l09dm0zgus <cx9ps3@gmail.com>
2023-09-21 23:15:27 +0300

    "Add detailed comments to exception classes in core module
    Improved understanding and readability of the 'ComponentNotFound' and 'MappingNotFound' classes in the core exceptions module. Detailed comments added to methods and attributes provide context and explanation of these classes' functionality. This is expected to ease the process for new maintainers and contributors, making codebase more comprehensible."

--- engine/src/core/exceptions/ComponentNotFound.hpp
+++ engine/src/core/exceptions/ComponentNotFound.hpp
@@ -6,14 +6,39 @@
 #include <string>
 namespace hexen::engine::exceptions
 {
+	/**
+ * @class ComponentNotFound
+ * @brief This is a custom exception class, which is useful when a specified component is not found.
+ * It inherits from standard exception class in C++.
+ */
 	class ComponentNotFound : public std::exception
 	{
 	public:
+		/**
+     	* @brief A constructor that takes a name of the component that was not found.
+     	* @param componentName A string representing the name of the component.
+     	*/
 		explicit ComponentNotFound(std::string componentName);
+
+		/**
+     	* @brief A method from std::exception, overridden to provide the custom error message.
+     	* @return A const char* which contains the error message.
+     	*/
+
 		[[nodiscard]] const char *what() const noexcept override;
 
 	private:
+		/**
+     	* @brief The name of the component which was not found.
+     	*/
+
 		std::string componentName;
+
+		/**
+     	* @brief The error message which is returned when the exception is thrown.
+     	*/
+
 		std::string message;
 	};
+	;
 }// namespace hexen::engine::exceptions
--- engine/src/core/exceptions/MappingNotFound.hpp
+++ engine/src/core/exceptions/MappingNotFound.hpp
@@ -7,18 +7,37 @@
 
 namespace hexen::engine::exceptions
 {
-	class MappingNotFound : public std::exception
-	{
+	/**
+	 * @class MappingNotFound
+	 * @brief Custom exception class that is thrown when a specific element is not found within a mapping input.
+	 * Inherits from the standard exception class.
+	 */
+
+	class MappingNotFound : public std::exception {
 	public:
+
+		/**
+		 * @brief Explicit constructor for the MappingNotFound class.
+		 *
+		 * @param mappingName Name of the mapping that was not found.
+		 * Initializes the mappingName data member and creates a custom error message.
+		 */
+
 		explicit MappingNotFound(const std::string &mappingName) : mappingName(mappingName)
 		{
 			message = "Error: Not founded axis/action mapping with this name : " + mappingName + " !\n";
 		};
 
-		[[nodiscard]] const char *what() const noexcept override;
+		/**
+		* @brief Override of the std::exception::what() method.
+		*
+		* @returns A C-style character string describing the general cause
+		*     of the current error (the same string passed to the ctor).
+		*/
 
+		[[nodiscard]] const char *what() const noexcept override;
 	private:
-		std::string mappingName;
-		std::string message;
+		std::string mappingName; /**< @brief Name of the mapping that was not found. */
+		std::string message; /**< @brief Custom error message to be displayed when exception is caught. */
 	};
 }// namespace hexen::engine::exceptions
