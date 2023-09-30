e25f61035f2c1d1a03baea0fc6836baca7ef0c55
l09dm0zgus <cx9ps3@gmail.com>
2023-09-30 22:25:36 +0300

    Add shared_ptr reinterpretation on Android
    To enable compatibility with Android, a reinterpret_pointer_cast has been added to interpret shared pointers correctly on this platform. This change is scoped within a conditional macro check for the Android environment. This step ensures correct interpretation and handling of shared pointers in multi-threaded operations in the Task.hpp file specifically for the Android platforms, improving cross-platform compatibility.

--- engine/src/core/threading/task/Task.hpp
+++ engine/src/core/threading/task/Task.hpp
@@ -1,6 +1,19 @@
 #pragma once
 #include "../../Types.hpp"
+#include <memory>
+#include <any>
 
+#ifdef __ANDROID__
+namespace std
+{
+    template<class T, class U>
+    std::shared_ptr<T> reinterpret_pointer_cast(const std::shared_ptr<U>& r) noexcept
+    {
+        auto p = reinterpret_cast<typename std::shared_ptr<T>::element_type*>(r.get());
+        return std::shared_ptr<T>{r, p};
+    }
+}
+#endif
 namespace hexen::engine::core::threading
 {
 	class TaskScheduler;
