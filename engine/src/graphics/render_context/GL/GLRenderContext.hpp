656880a43f5e0d6250fbd04d765c9cb8d312db91
l09dm0zgus <cx9ps3@gmail.com>
2023-10-05 18:10:37 +0300

    Update OpenGL render context documentation in header file
    This commit adds descriptive comments to the GLRenderContext class in the GLRenderContext.hpp header file. A detailed class description is added as well as thorough explanations for its methods and data members. The purpose of this is to improve readability and provide clarify on the function of each class component. Minor refactoring was also done to ensure consistency in the codebase.

--- engine/src/graphics/render_context/GL/GLRenderContext.hpp
+++ engine/src/graphics/render_context/GL/GLRenderContext.hpp
@@ -7,26 +7,63 @@
 #include <application/Settings.hpp>
 namespace hexen::engine::graphics::gl
 {
+	/**
+ 	* @class GLRenderContext
+ 	* @brief A class that represents an OpenGL rendering context.
+ 	* @details This class is responsible for handling OpenGL-specific implementation
+ 	*          details of a rendering context.
+ 	*/
+
 	class GLRenderContext : public RenderContext
 	{
 	private:
-
 		/**
-		* @brief  Represents OpenGL context.
-		*/
+     	* @brief Represents OpenGL context.
+     	*/
 
 		SDL_GLContext glContext {nullptr};
 
-		void setGLHints(const core::Settings& settings);
+		/**
+     	* @brief Sets hint related to OpenGL settings.
+     	* @param settings Contains different properties like resolution and full screen mode.
+     	*/
+
+		void setGLHints(const core::Settings &settings);
+
+		/**
+     	* @brief Initializes the GLEW library.
+     	* @note GLEW (OpenGL Extension Wrangler Library) is used to access modern OpenGL API features.
+     	*/
+
 		void initializeGLEW() const;
+
+		/**
+     	* @brief Represents the structure which is corresponding to the OpenGL settings.
+    	*/
 
 		core::Settings::OpenGLSettings openGlSettings;
 
 	protected:
-		[[nodiscard]] core::i32 getWindowFlags() const noexcept override;
-		void createRenderContextForSDLWindow(SDL_Window *window) override;
-	public:
-		explicit GLRenderContext(const core::Settings& settings);
+		/**
+     	* @brief Returns SDL window flags specific for OpenGL rendering context.
+     	* @return Integer value of window flags.
+     	*/
 
+		[[nodiscard]] core::i32 getWindowFlags() const noexcept override;
+
+		/**
+     	* @brief Creates actual SDL rendering context for the given window.
+     	* @param window A pointer to the SDL_Window structure, which is the window where rendering is done.
+     	*/
+
+		void createRenderContextForSDLWindow(SDL_Window *window) override;
+
+	public:
+		/**
+     	* @brief Constructs a GLRenderContext with specified settings.
+     	* @param settings Contains different properties like resolution and full screen mode.
+     	*/
+
+		explicit GLRenderContext(const core::Settings &settings);
 	};
-}
+}// namespace hexen::engine::graphics::gl
