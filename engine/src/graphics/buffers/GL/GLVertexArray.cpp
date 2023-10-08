071d0a9c3c53dea57064cc0f70759687e646e3d7
l09dm0zgus <cx9ps3@gmail.com>
2023-10-08 17:57:45 +0300

    Refactor GLVertexAttributes and update MessageBox, Buffer classes
    Removed GLVertexAttributes.cpp and GLVertexAttributes.hpp files as they were replaced with VertexArray.cpp and VertexArray.hpp. These changes were made to simplify the code and enhance clarity.
    
    In MessageBox.cpp, a return true statement was added to ensure proper termination of the function.
    
    In Buffers.hpp, changed unique_ptr to shared_ptr in ElementsBuffer, VertexBuffer and FrameBuffer classes to allow shared ownership, hence aiding in preventing memory leaks.
    
    And, removed default false assignment to "isNormalized" parameter in BufferElement's constructor - an optimization fix.
    
    Renamed and updated GLVertexArrayObject in the GLVertexArray class following changes in Buffer classes.
    
    Modifications in CMakeLists files were made to accommodate renamed and new files.

---
+++ engine/src/graphics/buffers/GL/GLVertexArray.cpp
@@ -0,0 +1,92 @@
+//
+// Created by cx9ps3 on 04.01.23.
+//
+
+#include "GLVertexArray.hpp"
+#ifndef __ANDROID__
+	#include "GL/glew.h"
+#else
+	#include <GLES3/gl31.h>
+#endif
+namespace hexen::engine::graphics
+{
+	GLenum shaderDataTypeToGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case ShaderDataType::NONE:
+				break;
+			case ShaderDataType::FLOAT:
+				return GL_FLOAT;
+			case ShaderDataType::INT:
+				return GL_INT;
+			case ShaderDataType::BOOL:
+				return GL_BOOL;
+			case ShaderDataType::VEC2F:
+				return GL_FLOAT;
+			case ShaderDataType::VEC3F:
+				return GL_FLOAT;
+			case ShaderDataType::VEC4F:
+				return GL_FLOAT;
+			case ShaderDataType::VEC2I:
+				return GL_INT;
+			case ShaderDataType::VEC3I:
+				return GL_INT;
+			case ShaderDataType::VEC4I:
+				return GL_INT;
+			case ShaderDataType::MAT2:
+				return GL_FLOAT;
+			case ShaderDataType::MAT3:
+				return GL_FLOAT;
+			case ShaderDataType::MAT4:
+				return GL_FLOAT;
+		}
+
+		HEXEN_ASSERT(false,"ERROR::Unknown ShaderDataType!");
+		return 0;
+	}
+
+
+}
+
+hexen::engine::graphics::gl::GLVertexArray::GLVertexArray()
+{
+	glGenVertexArrays(1, &object);
+}
+
+void hexen::engine::graphics::gl::GLVertexArray::bind() const
+{
+	glBindVertexArray(object);
+}
+
+hexen::engine::graphics::gl::GLVertexArray::~GLVertexArray()
+{
+	glDeleteVertexArrays(1, &object);
+}
+
+void hexen::engine::graphics::gl::GLVertexArray::unbind() const
+{
+	glBindVertexArray(0);
+}
+void hexen::engine::graphics::gl::GLVertexArray::addVertexBuffer(const std::shared_ptr<hexen::engine::graphics::VertexBuffer> &vertexBuffer)
+{
+	glBindVertexArray(object);
+	vertexBuffer->bind();
+	core::u32 index{0};
+
+	const auto& layout = vertexBuffer->getLayout();
+	for(const auto& element : layout)
+	{
+		glEnableVertexAttribArray(index);
+		glVertexAttribPointer(index,element.getComponentCount(), shaderDataTypeToGLBaseType(element.type),element.bIsNormalized ? GL_TRUE : GL_FALSE,layout.getStride(),(const core::vptr)element.offset);
+		index++;
+	}
+
+	vertexBuffers.push_back(vertexBuffer);
+}
+void hexen::engine::graphics::gl::GLVertexArray::setElementBuffer(const std::shared_ptr<ElementsBuffer> &newElementsBuffers)
+{
+	glBindVertexArray(object);
+	newElementsBuffers->bind();
+	elementsBuffer = newElementsBuffers;
+}
