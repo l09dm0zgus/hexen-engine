a60451de509780ee3f763fa9d6099e975881e2f6
l09dm0zgus <cx9ps3@gmail.com>
2023-11-03 20:15:14 +0200

    Add IAsset interface for in-memory asset representation
    Introduced an IAsset interface as a standard for creating Asset objects within a game engine. The interface provides a set of pure virtual methods to load, save and access general info about an asset. This interface should ensure a consistent API for all possible asset classes, making them easier to manage within containers, and at the same time, it provides flexibility for developers to create their propio asset subclasses. This change lays the foundation of the asset loading and management system in the engine.

---
+++ engine/src/core/assets/IAsset.hpp
@@ -0,0 +1,62 @@
+//
+// Created by cx9ps3 on 01.11.2023.
+//
+
+#pragma once
+#include "../memory_pool/AllocatedObject.hpp"
+#include <filesystem>
+
+namespace hexen::engine::core::assets
+{
+	/**
+ 	* @class IAsset
+ 	* @brief A class representing an Asset in memory.
+ 	*
+ 	* IAsset is designed as an interface with pure virtual methods
+ 	* that must be implemented in any derived classes.
+ 	* It represents an Asset with functions to load and save it,
+ 	* and accessors to get its name and raw data.
+ 	*
+ 	* @note This class inherits from the memory::AllocatedObject class.
+ 	*/
+
+	class IAsset : public memory::AllocatedObject
+	{
+	public:
+
+		/**
+    	* @brief Default Virtual Destructor.
+    	*/
+
+		virtual ~IAsset() = default;
+
+		/**
+    	* @brief Pure virtual method to get the name of the Asset.
+    	* @return The name of the Asset as a std::string.
+    	*/
+
+		[[nodiscard]] virtual std::string getName() const = 0;
+
+		/**
+    	* @brief Pure virtual method to get the raw data of the Asset.
+    	* @return The raw data of the Asset as a std::vector of core::u8.
+    	*/
+
+		[[nodiscard]] virtual std::vector<core::u8> getRawData() const = 0;
+
+		/**
+    	* @brief Pure virtual method to load the Asset from a given file path.
+    	* @param pathToAsset The file path to the Asset.
+    	*/
+
+		virtual void load(const std::filesystem::path &pathToAsset) = 0;
+
+		/**
+    	* @brief Pure virtual method to save the Asset to a given file path.
+    	* @param pathToAsset The file path to the location where the Asset and its raw file should be saved.
+    	* @param pathToRawFile The file path to the location where the raw file of the Asset should be saved.
+    	*/
+
+		virtual void save(const std::filesystem::path &pathToAsset, const std::filesystem::path &pathToRawFile) = 0;
+	};;
+}// namespace hexen::engine::core::asset
