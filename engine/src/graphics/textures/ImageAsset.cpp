d9b84c362eba7a8f345f29497309ab19d244d9fa
l09dm0zgus <cx9ps3@gmail.com>
2023-11-09 19:18:36 +0200

    Add ImageAsset class to manage image files
    The class ImageAsset has been added to 'engine/src/graphics/textures' directory and integrated within the engine. It is designed to handle image assets, by providing methods to load, save, retrieve raw data and other properties of image assets. The new class provides a more efficient way to manage images files, increase flexibility and streamline the game engine performance. The changes are reflected in 'ImageAsset.hpp' and 'ImageAsset.cpp' files and updated 'CMakeLists.txt' to include these files during compilation.

---
+++ engine/src/graphics/textures/ImageAsset.cpp
@@ -0,0 +1,103 @@
+//
+// Created by cx9ps3 on 09.11.2023.
+//
+
+#include "ImageAsset.hpp"
+#include <SDL_image.h>
+#include <fstream>
+
+void hexen::engine::graphics::ImageAsset::save(const std::filesystem::path &pathToAsset, const std::filesystem::path &pathToRawFile)
+{
+	HEXEN_ADD_TO_PROFILE();
+
+	auto *surface = IMG_Load(pathToRawFile.string().c_str());
+
+	if (surface == nullptr)
+	{
+		SDL_Log("Error : Failed to load %s image.", pathToRawFile.string().c_str());
+	}
+
+	auto format = surface->format;
+
+	constexpr core::u8 eightBitsPerPixel = 8;
+	constexpr core::u8 sixteenBitsPerPixel = 16;
+	constexpr core::u8 thirtyTwoBitsPerPixel = 32;
+
+	switch (format->BitsPerPixel)
+	{
+		case eightBitsPerPixel:
+			assetDataFile["image_asset"]["format"] = format->Amask ? ImageFormat::RGBA8 : ImageFormat::RGB8;
+			break;
+		case sixteenBitsPerPixel:
+			assetDataFile["image_asset"]["format"] = format->Amask ? ImageFormat::RGBA16 : ImageFormat::RGB16;
+			break;
+		case thirtyTwoBitsPerPixel:
+			assetDataFile["image_asset"]["format"] = format->Amask ? ImageFormat::RGBA32 : ImageFormat::RGB32;
+			break;
+		default:
+			assetDataFile["image_asset"]["format"] = ImageFormat::RGB8;
+			break;
+	}
+
+
+	//HEXEN_ASSERT(internalFormat & dataFormat, "Format not supported!");
+
+	auto height = surface->h;
+	auto width = surface->w;
+
+	assetDataFile["image_asset"]["size"]["width"] = width;
+	assetDataFile["image_asset"]["size"]["height"] = height;
+	assetDataFile["image_asset"]["name"] = pathToRawFile.filename();
+
+	const auto *imagePixels = static_cast<char *>(surface->pixels);
+	assetDataFile["image_asset"]["raw_data"] = imagePixels;
+
+	rawImageData.assign(imagePixels, imagePixels + strlen(imagePixels));
+
+	auto binaryJsonData = nlohmann::json::to_bson(assetDataFile);
+
+	std::ofstream outFile(pathToAsset, std::ios::binary);
+	outFile.write((char *) binaryJsonData.data(), binaryJsonData.size() * sizeof(core::u8));
+}
+
+std::vector<hexen::engine::core::u8> hexen::engine::graphics::ImageAsset::getRawData() const
+{
+	HEXEN_ADD_TO_PROFILE();
+	return rawImageData;
+}
+
+hexen::engine::core::i32 hexen::engine::graphics::ImageAsset::getHeight() const
+{
+	HEXEN_ADD_TO_PROFILE();
+	return assetDataFile["image_asset"]["size"]["height"];
+}
+
+hexen::engine::core::i32 hexen::engine::graphics::ImageAsset::getWidth() const
+{
+	HEXEN_ADD_TO_PROFILE();
+	return assetDataFile["image_asset"]["size"]["width"];
+}
+std::string hexen::engine::graphics::ImageAsset::getName() const
+{
+	HEXEN_ADD_TO_PROFILE();
+	return assetDataFile["image_asset"]["name"];
+}
+
+void hexen::engine::graphics::ImageAsset::load(const std::filesystem::path &pathToAsset)
+{
+	HEXEN_ADD_TO_PROFILE();
+	std::ifstream const inFile(pathToAsset, std::ios::binary);
+	std::stringstream ss;
+	ss << inFile.rdbuf();
+
+	assetDataFile = nlohmann::json::from_bson(ss);
+	auto imagePixels = assetDataFile["image_asset"]["raw_data"];
+
+	rawImageData.assign(imagePixels.cbegin(), imagePixels.cend());
+}
+
+hexen::engine::graphics::ImageAsset::ImageFormat hexen::engine::graphics::ImageAsset::getFormat() const
+{
+	HEXEN_ADD_TO_PROFILE();
+	return static_cast<ImageFormat>(assetDataFile["image_asset"]["format"]);
+}
---
+++ engine/src/graphics/textures/ImageAsset.hpp
@@ -0,0 +1,111 @@
+//
+// Created by cx9ps3 on 09.11.2023.
+//
+
+#pragma once
+#include <assets/IAsset.hpp>
+#include <nlohmann/json.hpp>
+
+namespace hexen::engine::graphics
+{
+	/**
+ 	* @class ImageAsset
+ 	* @brief Handles image assets, inheriting from IAsset interface
+ 	* @extends IAsset
+ 	* @details This class provides handling for image files of various formats. It provides methods
+ 	*  to load, save, retrieve raw data and other properties of image assets.
+ 	*/
+
+	class ImageAsset : public core::assets::IAsset
+	{
+	private:
+		/** @brief JSON file for storing asset data */
+		nlohmann::json assetDataFile;
+
+		/** @brief Raw image data as vector of unsigned chars */
+		std::vector<core::u8> rawImageData;
+
+	public:
+		/**
+    	* @enum ImageFormat
+    	* @brief Supported image formats
+    	*/
+
+		enum class ImageFormat : core::u8
+		{
+			RGB8,  ///< 8-bit RGB format
+			RGBA8, ///< 8-bit RGBA format
+			RGB16, ///< 16-bit RGB format
+			RGBA16,///< 16-bit RGBA format
+			RGB32, ///< 32-bit RGB format
+			RGBA32,///< 32-bit RGBA format
+		};
+
+		/**
+    	* @brief Get the format of the image.
+    	* @return Enum representing the format of the image.
+    	*/
+
+		[[nodiscard]] ImageFormat getFormat() const;
+
+		/**
+    	* @brief Get the name of the Asset.
+    	* @return The name of the Asset as a std::string.
+    	*/
+
+		[[nodiscard]] std::string getName() const override;
+
+		/**
+    	* @brief Get the raw data of the Asset.
+    	* @return The raw data of the Asset as a std::vector of core::u8.
+    	*/
+
+		[[nodiscard]] std::vector<core::u8> getRawData() const override;
+
+		/**
+         * @brief Loads an image asset from the specified filesystem path.
+         *
+         * This function reads the specified file as binary, loads its contents into a stringstream,
+         * then converts the stringstream content from BSON to JSON format.
+         * The raw image data is then retrieved from the JSON object and assigned to rawImageData.
+         * In case of any errors during the process, appropriate exceptions are thrown.
+         *
+         * @param pathToAsset The filesystem path of the image asset to be loaded.
+         */
+
+		void load(const std::filesystem::path &pathToAsset) override;
+
+		/**
+ 		* @brief Saves an image as an asset.
+ 		*
+ 		* This function reads an image file from the specified pathToRawFile, determines its height, width,
+ 		* and format, and writes this data along with the image's raw pixel data to an asset file at the specified pathToAsset.
+ 		*
+ 		* @param[in] pathToAsset  The filesystem path where the asset file will be saved.
+ 		* @param[in] pathToRawFile The filesystem path where the raw image file is stored.
+ 		*
+ 		* @warning If the loading of the image file fails, the function will log an error message using SDL_Log.
+ 		*          It uses JSON for BSON to serialize the asset data and write it to the asset file.
+ 		*
+ 		* @note The function automatically detects the format of the image based on its pixel depth.
+ 		*       Supported formats are RGB8, RGBA8, RGB16, RGBA16, RGB32, and RGBA32.
+ 		*       If the format does not correspond to any of the supported formats, it will default to RGB8.
+ 		*/
+
+		void save(const std::filesystem::path &pathToAsset, const std::filesystem::path &pathToRawFile) override;
+
+		/**
+    	* @brief Get the height of the image.
+    	* @return The height of the image.
+    	*/
+
+		core::i32 getHeight() const;
+
+		/**
+    	* @brief Get the width of the image.
+    	* @return The width of the image.
+    	*/
+
+		core::i32 getWidth() const;
+	};
+}// namespace hexen::engine::graphics
